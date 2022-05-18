c140f9be441ba7631521a14c408bc56881da261e
smitpatel-03 <patelsmitkumar03@gmail.com>
2022-05-18 12:19:34 +0530

    dd-csv-parser-and-read-file-data

---
+++ index.js
@@ -0,0 +1,20 @@
+const parse = require("csv-parse");
+const fs = require("fs");
+// const { createReadStream } = require("fs");
+
+const result = [];
+
+fs.createReadStream('kepler_data.csv')
+    .on("data", (data) => {
+        result.push(data);
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on("end", () => {
+        console.log(result);
+        console.log("end")
+    });
+
+
+
